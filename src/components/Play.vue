5c8b2156b2f315f39dc0ab8b9357874e5aae9b33
Monish Raaj <cb.en.u4cse18239@cb.students.amrita.edu>
2019-12-16 02:19:10 +0530

    styling added

--- src/components/Play.vue
+++ src/components/Play.vue
@@ -1,27 +1,27 @@
 <template>
   <div>
     <div class="inputWrapper">
-      <h1 v-if="command===1">ON YOUR MARKS!</h1>
-      <h1 v-else-if="command===2">GET SET!</h1>
-      <h1 v-else-if="command===3">GO!</h1>
+      <h1 v-if="command===1" class="background1">ON YOUR MARKS!</h1>
+      <h1 v-else-if="command===2" class="background2">GET SET!</h1>
+      <h1 v-else-if="command===3" class="background3">GO!</h1>
       <!-- <h1 v-else></h1> -->
     </div>
-    <div align='center'>
+    <div align="center">
       <div v-if="typingcontents" class="centerbox">
-      <h6>wpm:{{wpm}}</h6>
-      <span>{{finished}}</span>
-      <span>{{currentfinished}}</span>
-      <span>{{currenterror}}</span>
-      <span class="blinking-cursor">|</span>
-      <span>{{currentfinishedafter}}</span>
-      <span>
-        <u>{{currentunfinished}}</u>
-      </span>
-      <span>{{unfinished}}</span>
-      <br>
-      <input type="text" id="tempword" v-model="tempword" :disabled='disableInput' />
-      <p>{{tempword}}</p>
-    </div>
+        <h6>wpm:{{wpm}}</h6>
+        <span>{{finished}}</span>
+        <span>{{currentfinished}}</span>
+        <span>{{currenterror}}</span>
+        <span class="blinking-cursor">|</span>
+        <span>{{currentfinishedafter}}</span>
+        <span>
+          <u>{{currentunfinished}}</u>
+        </span>
+        <span>{{unfinished}}</span>
+        <br />
+        <input type="text" id="tempword" v-model="tempword" :disabled="disableInput" />
+        <p>{{tempword}}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -39,7 +39,7 @@ export default {
       currenterror: null,
       unfinished: null,
       wpm: 0,
-      disableInput:true
+      disableInput: true
     };
   },
   watch: {
@@ -48,7 +48,7 @@ export default {
   methods: {
     Focus(location) {
       document.getElementById(location).focus();
-    },
+    }
   },
   created() {
     this.typingcontents = "something to type";
@@ -57,8 +57,8 @@ export default {
     setInterval(function() {
       self.command++;
       if (self.command >= 3) {
-        self.disableInput=false;
-        self.Focus('tempword');
+        self.disableInput = false;
+        self.Focus("tempword");
         clearInterval();
       }
     }, 1000);
@@ -66,12 +66,11 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .inputWrapper {
   display: inline-block;
   width: 100%;
   text-align: center;
-  background-color: blue;
   color: aliceblue;
 }
 .blinking-cursor {
@@ -84,17 +83,29 @@ export default {
   -o-animation: 1s blink step-end infinite;
   animation: 1s blink step-end infinite;
 }
-.centerbox{
+.centerbox {
   display: inline-block;
-  width:50%;
-  height:40%;
-  background-color: blanchedalmond;
+  width: 50%;
+  height: 40%;
+  background-color: #000;
   margin: 0;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  position:absolute;
+  position: absolute;
   border-radius: 100px;
   overflow: auto;
+}
+.background1 {
+  background-color: #dc3545;
+  color: black;
+}
+.background2 {
+  color: black;
+  background-color: #ffff00;
+}
+.background3 {
+  color: black;
+  background-color: #39ff14;
 }
 </style>
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,18 +1,39 @@
 <template>
-  <div>
-    <p>welcome</p>
-    <router-link tag="button" to="/play" class="btn btn-primary btn-danger">Start Typing</router-link>
+  <div >
+    <div align="center">
+      <div class="centerbox">
+        <h1>AM TouchTyper</h1>
+        <router-link tag="button" to="/play" class="btn btn-primary btn-danger">Start Typing</router-link>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'jquery'
-import 'popper.js'
-export default {
-
-};
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "jquery";
+import "popper.js";
+export default {};
 </script>
 
-<style>
+<style scoped>
+.centerbox {
+  border-radius: 25px;
+  background-color: black;
+  color:aliceblue;
+  margin: 0;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  overflow: auto;
+  height:50%;
+  padding: 4%;
+}
+button{
+  position:fixed;
+  top: 50%;
+  left:50%;
+  transform: translate(-50%, -50%);
+}
 </style>
